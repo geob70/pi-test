a489ed084d559f5def4fbef59eb52e335028b6d4
Sylvester George <sylvestergeorge@Sylvesters-MBP.cust.communityfibre.co.uk>
2023-11-25 01:17:16 +0000

    allow all host

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g+=a)7vh_!e9i@44jb&+dx6*b94e_ekl+51q2$79&)(0l8s0m&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
