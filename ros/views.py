6729141fbcc7d025db97280de20557af3747e862
Sylvester George <sylvestergeorge@Sylvesters-MBP.cust.communityfibre.co.uk>
2023-11-25 18:18:06 +0000

    router os test

--- ros/views.py
+++ ros/views.py
@@ -1,6 +1,8 @@
 from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework import status, permissions, viewsets
 from rest_framework.decorators import action
+import ros_api
 
 
 class RosViews(viewsets.ModelViewSet):
@@ -15,6 +17,18 @@ class RosViews(viewsets.ModelViewSet):
         serializer_class=[],
         url_path="ping",
     )
-    def ping(self, request) -> Response:
+    def ping(self, request: Request) -> Response:
         """Test"""
         return Response({"message": "Good response"}, status=status.HTTP_200_OK)
+
+    def talk_ros(self, request: Request) -> Response:
+        try:
+            router = ros_api.Api('172.16.10.1', user='Lantore', password='1')
+            r = router.talk('/system/identity/print')
+            # hd508aezw2m.sn.mynetname.net
+            # 102.215.57.75
+
+            # print(r)
+            return Response({'data': r}, status=status.HTTP_200_OK)
+        except ValueError as error:
+            return Response({'Error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
