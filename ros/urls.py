469f88f63298885f844da71782c9cfd2dc7acde6
Sylvester George <sylvestergeorge@Sylvesters-MBP.cust.communityfibre.co.uk>
2023-11-28 20:03:25 +0000

    Added fetch profile

--- ros/urls.py
+++ ros/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from ros import views
 
 urlpatterns = [
-    path('ros/ping', views.RosViews.as_view({'get': 'ping'})),
-    path('ros/talk', views.RosViews.as_view({'get': 'talk_ros'})),
+    path("ros/ping", views.RosViews.as_view({"get": "ping"})),
+    path("ros/profile", views.RosViews.as_view({"post": "profile", "get": "profile"})),
+    path(
+        "ros/hotspot-user",
+        views.RosViews.as_view({"post": "hotspot_user", "get": "hotspot_user"}),
+    ),
 ]
--- ros/views.py
+++ ros/views.py
@@ -18,43 +18,102 @@ class RosViews(viewsets.ModelViewSet):
         url_path="ping",
     )
     def ping(self, request: Request) -> Response:
-        """Test"""
+        """Check raspberry server avalability"""
         return Response({"message": "Good response"}, status=status.HTTP_200_OK)
 
     @action(
-        methods=["GET"],
+        methods=["POST", "GET"],
         detail=False,
         permission_classes=[permissions.AllowAny],
         serializer_class=[],
-        url_path="talk",
+        url_path="profile",
     )
-    def talk_ros(self, request: Request) -> Response:
-        try:
-            connection = routeros_api.RouterOsApiPool(
-                host="172.16.10.1",
-                username="Lantore",
-                password="1",
-                plaintext_login=True,
-            )
-            api = connection.get_api()
+    def profile(self, request: Request) -> Response:
+        """Hotspot profile with limitations"""
+        connection = routeros_api.RouterOsApiPool(
+            host="172.16.10.1",
+            username="Lantore",
+            password="1",
+            plaintext_login=True,
+        )
+        api = connection.get_api()
 
-            # Add a profile with the desired limitations
-            profile = api.get_resource("/ip/hotspot/user/profile").add(
-                name="apiprofile", rate_limit="256k/512k", shared_users="10"
-            )
+        if request.method == "POST":
+            # All fields are in string format
+            profile_name = request.data["profile_name"]
+            rate_limit = request.data["rate_limit"]
+            shared_users = request.data["shared_users"]
 
-            # Add a user with the desired profile and limitations
-            user = api.get_resource("/ip/hotspot/user").add(
-                name="apiuserT", password="password1", profile="apiprofile"
-            )
+            try:
+                # Add a profile with the desired limitations
+                profile = api.get_resource("/ip/hotspot/user/profile").add(
+                    name=profile_name, rate_limit=rate_limit, shared_users=shared_users
+                )
 
-            print(user)
+                # Close the connection
+                connection.disconnect()
 
-            # Close the connection
-            connection.disconnect()
+                return Response(
+                    {"message": "Profile created successfully", "data": request.data},
+                    status=status.HTTP_200_OK,
+                )
+            except ValueError as error:
+                return Response(
+                    {"Error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
 
-            return Response({"data": "user"}, status=status.HTTP_200_OK)
-        except ValueError as error:
-            return Response(
-                {"Error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-            )
+        if request.method == "GET":
+            # Fetch List/Resource
+            list = api.get_resource("/ip/hotspot/user/profile")
+            profiles = list.get()
+            print(profiles)
+
+            return Response({"profiles": profiles}, status=status.HTTP_200_OK)
+
+    @action(
+        methods=["POST", "PATCH"],
+        detail=False,
+        permission_classes=[permissions.AllowAny],
+        serializer_class=[],
+        url_path="hotspot-user",
+    )
+    def hotspot_user(self, request: Request) -> Response:
+        """Create a hotspot user profile"""
+        connection = routeros_api.RouterOsApiPool(
+            host="172.16.10.1",
+            username="Lantore",
+            password="1",
+            plaintext_login=True,
+        )
+        api = connection.get_api()
+
+        if request.method == "PATCH":
+            # Update User Profile
+            user_profile = api.get_resource("/ip/hotspot/user/profile")
+            user_profile.set(id="1", name="new_name", rate_limit="10M/10M")
+            return Response({"message": "yoooo"}, status=status.HTTP_200_OK)
+
+        if request.method == "POST":
+            try:
+                # All fields are in string format
+                profile_name = request.data["profile_name"]
+                password = request.data["password"]
+                user_name = request.data["user_name"]
+
+                # Add a user with the desired profile and limitations
+                user = api.get_resource("/ip/hotspot/user").add(
+                    name=user_name, password=password, profile=profile_name
+                )
+
+                print(user)
+
+                # Close the connection
+                connection.disconnect()
+
+                return Response(
+                    {"message": "user created successfully"}, status=status.HTTP_200_OK
+                )
+            except ValueError as error:
+                return Response(
+                    {"Error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
