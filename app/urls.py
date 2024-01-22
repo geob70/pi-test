d307ccaaf7f1e396241a1fb7f8b857f0b1eefc1c
Sylvester George <sylvestergeorge@Sylvesters-MBP.cust.communityfibre.co.uk>
2024-01-22 11:55:13 +0000

    update

--- app/urls.py
+++ app/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('orange/admin/', admin.site.urls),
-    path('orange/api/', include('ros.urls')),
+    path('admin/', admin.site.urls),
+    path('api/', include('ros.urls')),
 ]
