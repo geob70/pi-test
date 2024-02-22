95bf0f9d33277a0ff99744b106a5cdede783c7f8
Sylvester George <sylvestergeorge@Sylvesters-MBP.cust.communityfibre.co.uk>
2024-02-22 19:14:27 +0000

    data usage

--- ros/views.py
+++ ros/views.py
@@ -260,7 +260,11 @@ def check_data_usage(request: Request) -> Response:
             # user_resource = api.get_resource('/user')
             # user_resource.set(id=user['id'], disabled='yes')
             # api.get_resource("/ip/hotspot/user").set(name=username, disabled="true")
-            api.get_resource("/ip/hotspot/user").set(id=user_id, disabled="no")
+            api.get_resource("/ip/hotspot/user").set(
+                id=user_id,
+                disabled="no",
+                bytes_in="0",
+            )
             connection.disconnect()
             return Response(
                 {"data_used": data_used, "disabled": True}, status=status.HTTP_200_OK
